8a5542384c12997e6aff971ef6ef53f5eb381834
benber86 <benoitb@tutanota.de>
2022-08-27 19:45:02 +1000

    Add nginx

---
+++ docker/nginx/certbot.sh
@@ -0,0 +1,6 @@
+docker run -it --rm --name certbot \
+            -p 80:80 -p 443:443 \
+            -v "$(pwd)/cert:/etc/letsencrypt" \
+            certbot/certbot certonly --standalone -d momotaro.xyz \
+                 --non-interactive --agree-tos \
+                 --email admin@momotaro.xyz --expand
